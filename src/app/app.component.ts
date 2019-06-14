3963fd07e4cbb7e30a8dc721f57793d1e2f7e6ae
Tessa Sullivan <tessa.sullivan@gmail.com>
2019-06-14 10:04:27 -0700

    Added some baseline data to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { Item } from './models/item.model';
+import { Section } from './models/section.model';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-lifewire';
+  title = 'Lifewire Angular Repro';
+
+  // Data section - to be replaced with Firebase at a later date
+
+  // Header
+
+  // Sections
+
+  howToItem1: Item = new Item ("Article", "Start Creating with Canva today", "#link");
+  howToItem2: Item = new Item ("List", "Shooting Camera Raw can be a Game Changer", "#link");
+  howToItem3: Item = new Item ("Article", "Download Pictures from Your Computer", "#link");
+
+  howToSection: Section = new Section
+    ("How To", "icon", "photo", [this.howToItem1, this.howToItem2, this.howToItem3], "#link" );
+
+  fixItem1: Item = new Item("Article", "Wi-Fi Network Not Showing Up", "#link");
+  fixItem2: Item = new Item("Article", "7 Ways to Fix When Chrome is Freezing", "#link");
+  fixItem3: Item = new Item("Article", "Is Your Alexa App Offline or is it You?", "#link");
+
+  fixSection: Section = new Section 
+    ("Fix", "icon", "photo", [this.fixItem1, this.fixItem2, this.fixItem3], "#link");
+
+  buyItem1: Item = new Item("List", "The Best Places to Buy Phones in 2019", "#link");
+  buyItem2: Item = new Item("List", "Great Speakers for Outdoor Sound", "#link");
+  buyItem3: Item = new Item("List", "Best Xbox One Role Playing Games", "#link");
+
+  buySection: Section = new Section
+    ("Buy", "icon", "photo", [this.buyItem1, this.buyItem2, this.buyItem3], "#link");
+  
+
+  // Editor's pick
+
+  // Footer
 }
