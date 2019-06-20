3468a53c140808816fa5ae81ef5880521091611e
Tessa Sullivan <tessa.sullivan@gmail.com>
2019-06-20 15:38:25 -0700

    downgrade to Angular 1.6.5 - build works

---
+++ src/app/api-keys.ts
@@ -0,0 +1,9 @@
+export const masterFirebaseConfig = {
+    apiKey: "AIzaSyCi2J2ArYQ0zJis64ZLs7EKnM8dCsZiWkI",
+    authDomain: "lifewire-20244.firebaseapp.com",
+    databaseURL: "https://lifewire-20244.firebaseio.com",
+    projectId: "lifewire-20244",
+    storageBucket: "lifewire-20244.appspot.com",
+    messagingSenderId: "611366304719",
+    appId: "1:611366304719:web:a05316a66e2baa95"
+  };
